fb536424227e356c5abafa69ab7148814af087de
Dan Winship <danwinship@redhat.com>
2024-12-21 16:45:05 -0500

    Rename proxy.Provider to proxy.Proxier
    Originally, proxy.Provider represented the backend as a whole, but
    these days it represents only a single-stack proxier, which may just
    be half of the backend. Rename it to Proxier, which doesn't imply
    "this is the whole backend" quite as much (and matches the way we
    usually refer to that type anyway).

--- cmd/kube-proxy/app/server.go
+++ cmd/kube-proxy/app/server.go
@@ -175,7 +175,7 @@ type ProxyServer struct {
 	podCIDRs    []string // only used for LocalModeNodeCIDR
 	NodeManager *proxy.NodeManager
 
-	Proxier proxy.Provider
+	Proxier proxy.Proxier
 }
 
 // newProxyServer creates a ProxyServer based on the given config
--- pkg/proxy/ipvs/proxier.go
+++ pkg/proxy/ipvs/proxier.go
@@ -130,7 +130,7 @@ func NewDualStackProxier(
 	scheduler string,
 	nodePortAddresses []string,
 	initOnly bool,
-) (proxy.Provider, error) {
+) (proxy.Proxier, error) {
 	// Create an ipv4 instance of the single-stack proxier
 	ipv4Proxier, err := NewProxier(ctx, v1.IPv4Protocol, ipts[v1.IPv4Protocol], ipvs, ipset, sysctl,
 		syncPeriod, minSyncPeriod, filterCIDRs(false, excludeCIDRs), strictARP,
@@ -249,8 +249,8 @@ type Proxier struct {
 	logger klog.Logger
 }
 
-// Proxier implements proxy.Provider
-var _ proxy.Provider = &Proxier{}
+// Proxier implements proxy.Proxier
+var _ proxy.Proxier = &Proxier{}
 
 // NewProxier returns a new single-stack IPVS proxier.
 func NewProxier(
